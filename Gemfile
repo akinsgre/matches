fdd010a15c085d49434fb6d693ee4f683b88115c
Greg Akins <angrygreg@gmail.com>
2012-01-24 11:23:16 -0800

    Adding the ability to parse XML from http response and updating monitor to use a ThingSpeak URL to get river levels (instead of CPU temp)

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'sqlite3'
 gem 'annotate', '~> 2.4.1.beta'
 gem 'jquery-rails'
 gem 'therubyracer', '0.9.9'
-
+gem 'nokogiri', '1.5.0'
 group :assets do
   gem 'sass-rails',   '~> 3.1.5'
   gem 'coffee-rails', '~> 3.1.1'
--- monitor.rb
+++ monitor.rb
@@ -1,15 +1,25 @@
 require 'net/http'
+require 'nokogiri'
 
 dir = File.dirname(__FILE__)
 local_filename = dir + "/temps.txt"
 
   while true do
-    output = %x[/Applications/TemperatureMonitor.app//Contents/MacOS/tempmonitor -f | awk '{print $1}']  
+    geturi = URI.parse "http://api.thingspeak.com/apps/thinghttp/send_request?api_key=6NC5BBE84EO1VZFY"
+    http = Net::HTTP.new(geturi.host)
+    request = Net::HTTP::Get.new(geturi.request_uri)
+    request["Content-Type"] = "text/xml"
+    response = http.request(request)
+    doc = Nokogiri::XML(response.body)
+    output = doc.text
+    
+#    output = %x[/Applications/TemperatureMonitor.app//Contents/MacOS/tempmonitor -f | awk '{print $1}']  
 
-    uri = URI.parse("http://api.thingspeak.com/update")
-    post_args = {"key" => "ZU057XDYNZ1Q1U44", "field1" => output}
-    response = Net::HTTP.post_form(uri, post_args)
+   posturi = URI.parse("http://localhost:3000/update")
+    post_args = {"key" => "2X4DM9L8HI48AHDT", "field1" => output}
+    response = Net::HTTP.post_form(posturi, post_args)
     puts output + " posted as " + response.code.to_s
+
     sleep 900
   end
 
